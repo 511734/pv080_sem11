909a5ddfec2cf86d14efab3be7684f0b32f926b3
Jakub Kováč <511734@mail.muni.cz>
2022-04-28 11:13:00 +0200

    try2

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,7 +26,7 @@ class RunBinSh(object):
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
-    exec("import urllib%s as urllib" % version)
+    exec("import 'urllib%s' as urllib" % version)
 
 @app.route('/')
 def index():
