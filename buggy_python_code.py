73db865e809e88393e1fdb9705492efc6c9c3c65
Jakub Kováč <511734@mail.muni.cz>
2022-04-28 10:44:21 +0200

    some fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import cPickle
 import subprocess
 import base64
@@ -13,7 +14,8 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def authorized(request, user):
+    
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -31,5 +33,3 @@ def index():
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
-
-print(base64.b64encode(pickle.dumps(RunBinSh())))
